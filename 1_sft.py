150c81810627c0b8290aebae449fa85f4ce20ff6
Aksh Singhal <akshsinghal1004@gmail.com>
2026-03-15 16:00:41 +0530

    Remove dataset_text_field from SFTTrainer initialization
    Removed unused dataset_text_field parameter from SFTTrainer.

--- 1_sft.py
+++ 1_sft.py
@@ -95,7 +95,6 @@ def main():
         args=training_args,
         train_dataset=dataset,
         processing_class=tokenizer,
-        dataset_text_field="text",
     )
 
     # Use tqdm to show training progress
